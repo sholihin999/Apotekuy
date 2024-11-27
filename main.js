0c69de2b08c13b3b5656efb843281afd0972153d
sholihin999 <muhamadsolihin0857@gmail.com>
2024-11-27 20:27:45 +0700

    revisi

--- main.js
+++ main.js
@@ -38,6 +38,35 @@ document.addEventListener('DOMContentLoaded', function () {
         homeSection.scrollIntoView({ behavior: 'smooth' });
     });
 
+    // FAQ toggle functionality
+    const faqItems = document.querySelectorAll('.faq-item');
+    faqItems.forEach(item => {
+        const question = item.querySelector('.faq-question');
+        question.addEventListener('click', () => {
+            item.classList.toggle('active');
+        });
+    });
+
     window.addEventListener('scroll', checkVisibility);
     checkVisibility();
+});
+
+// Pilih semua kartu
+const cards = document.querySelectorAll('.card');
+
+// Tambahkan event listener untuk klik pada setiap kartu
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+        // Hapus kelas 'clicked' dari semua kartu lainnya
+        cards.forEach(c => c.classList.remove('clicked'));
+
+        // Tambahkan kelas 'clicked' ke kartu yang diklik
+        card.classList.add('clicked');
+    });
+});
+
+
+AOS.init({
+    duration: 800, // Durasi animasi dalam milidetik
+    once: true,    // Animasi terjadi sekali saja
 });
